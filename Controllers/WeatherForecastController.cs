e5eba0401ab60c8c8192fd0e43e1ab93f6592f78
efejotax08 <efejotax08@gmail.com>
2025-12-09 16:26:06 -0500

    Mas datos

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ExternalWebApi.Controllers
     {
         private static readonly string[] Cities = new[]
         {
-            "Quito", "Guayaquil", "Cuenca", "Ambato", "Loja"
+            "Quito", "Guayaquil", "Cuenca", "Ambato"
         };
 
         private static readonly Random rng = new Random();
@@ -16,7 +16,7 @@ namespace ExternalWebApi.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var weatherData = Enumerable.Range(1, 200).Select(index => new
+            var weatherData = Enumerable.Range(1, 400).Select(index => new
             {
                 fecha = DateTime.Now.AddDays(index).ToString("yyyy-MM-dd"),
                 ciudad = Cities[rng.Next(Cities.Length)],
