fcf84d6f459902a913233e25bc28639de8067a97
efejotax08 <efejotax08@gmail.com>
2025-12-10 09:48:28 -0500

    espacio

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -28,6 +28,7 @@ namespace ExternalWebApi.Controllers
                 fecha = fecha,
                 ciudad = Cities[rng.Next(Cities.Length)],
                 temperatura = rng.Next(10, 30), //mas
+
                 humedad = rng.Next(40, 90),
                 precipitacion = rng.NextDouble() < 0.3 ? rng.Next(0, 10) : 0
             },
